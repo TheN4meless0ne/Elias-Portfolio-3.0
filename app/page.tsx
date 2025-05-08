0650c76c985d3af2b6c3fe036f2eace34e5bfa90
TheN4meless0ne <thenmelessne@gmail.com>
2025-05-08 13:35:35 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Page() {
           className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
           rel="noopener noreferrer"
           target="_blank"
-          href="https://github.com/TheN4meless0ne"
+          href="https://www.sandnes.kommune.no/"
         >
           <FaIcon icon="fa-solid fa-location-dot" />
           <p className="ml-2 h-7">Sandnes, Norway</p>
@@ -21,7 +21,7 @@ export default function Page() {
           className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
           rel="noopener noreferrer"
           target="_blank"
-          href="https://github.com/TheN4meless0ne"
+          href="https://www.vaagen.vgs.no/hovedmeny/utdanningstilbud/informasjonsteknologi-og-medieproduksjon/"
         >
           <FaIcon icon="fa-solid fa-graduation-cap" />
           <p className="ml-2 h-7">Vågen Upper Secondary</p>
