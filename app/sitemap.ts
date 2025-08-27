d278dc8a4c14c0604297dfad38628992932e81f1
Elias Greig-Vistnes <159443781+TheN4meless0ne@users.noreply.github.com>
2025-08-27 15:35:36 +0200

    Fix: Replace getBlogPosts with getDocuments in sitemap
    Updated the sitemap generation to use getDocuments instead of getBlogPosts, ensuring all relevant documents are included in the sitemap output.

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,9 +1,9 @@
-import { getBlogPosts } from 'app/documents/utils'
+import { getDocuments } from 'app/documents/utils'
 
 export const baseUrl = 'https://portfolio-blog-starter.vercel.app'
 
 export default async function sitemap() {
-  let blogs = getBlogPosts().map((post) => ({
+  let blogs = getDocuments().map((post) => ({
     url: `${baseUrl}/blog/${post.slug}`,
     lastModified: post.metadata.publishedAt,
   }))
